76513a60c7a0ea647264ab955d16e6b3e2293b2e
Maxim Moskalyk <ictic.step@gmail.com>
2023-04-15 19:33:57 +0300

    Initial commit

---
+++ Flying Beaver IDE/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Flying_Beaver_IDE.Services;
+
+namespace Flying_Beaver_IDE
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            SyncfusionActivator.Activate();
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
---
+++ Flying Beaver IDE/Services/SyncfusionActivator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Syncfusion.Licensing;
+
+namespace Flying_Beaver_IDE.Services
+{
+    internal static class SyncfusionActivator
+    {
+        private const string LicenceKey = "SyncfusionKey";
+
+        public static void Activate()
+        {
+            var license = Environment.GetEnvironmentVariable(LicenceKey);
+            if(license is null)
+                return;
+            
+            SyncfusionLicenseProvider.RegisterLicense(license);
+        }
+    }
+}
